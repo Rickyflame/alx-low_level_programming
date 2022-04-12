3c981fe3aa1a36c593c395c65630d4e08c52b781
Rickyflame <kimumuricky@gmail.com>
2022-04-12 20:23:28 +0300

    Write a function that adds two itegers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,11 @@
 
 /**
  * add - addition of two integers
- * @x - first number.
- * @y - second number.
+ * @x: first number.
+ * @y: second number.
  * Return: returns x + j.
  */
 int add(int x, int y)
 {
 	return (x + y);
-}	
+}
