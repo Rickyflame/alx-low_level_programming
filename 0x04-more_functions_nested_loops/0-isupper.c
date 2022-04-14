09307f48121407c2520c79229aa3f6c139379b1a
Rickyflame <kimumuricky@gmail.com>
2022-04-14 14:39:32 +0300

    Write a function that checks for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks is c is uppercase or lowercase
+ * _isupper - checks if c is uppercase or lowercase
  * @c: representation of the character
  * Return: 1 or 0
  */
