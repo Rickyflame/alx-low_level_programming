866d5203a586ec1f6972d1a9c4f88bc917365f2b
Rickyflame <kimumuricky@gmail.com>
2022-04-14 19:52:18 +0300

    The “Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100, fizz and buzz replacing some numbers
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
