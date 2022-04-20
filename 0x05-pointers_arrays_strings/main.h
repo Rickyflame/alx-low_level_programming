90e0875f92ef4a5a3ce34381a42ad54dd2e392c9
Rickyflame <kimumuricky@gmail.com>
2022-04-20 13:01:04 +0300

    Write a function that prints a string, follwed by a new line, to stdout

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+/*prints _putchar*/
+int _putchar (char c);
+
 /* function that points to a int as parameter*/
 void reset_to_98(int *n);
 
@@ -6,3 +9,6 @@ void swap_int(int *a, int *b);
 
 /* function that returns the length of a string*/
 int _strlen(char *s);
+
+/*function that prints a string */
+void _puts(char *str);
