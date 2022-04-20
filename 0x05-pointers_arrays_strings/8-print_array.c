39e63d6ee9a8f0b8d994903f74954e95f80f668e
Rickyflame <kimumuricky@gmail.com>
2022-04-20 16:45:40 +0300

    Write a function that prints n elements of an array of integers, followed by a new line

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an arrayof integers
+ * @a: first integer
+ * @n: second integer
+ * void has no return value
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (;i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,3 +24,6 @@ void puts2(char *str);
 
 /*function that prints half of a string*/
 void puts_half(char *str);
+
+/*function prints n elements of an array of integers*/
+void print_array(int *a, int n);
