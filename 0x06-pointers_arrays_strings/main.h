10d286adb330d57a9a2a7a9d7e7ddc5592930e31
Rickyflame <kimumuricky@gmail.com>
2022-04-22 14:23:55 +0300

    Write a funtion that copies a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,7 @@ char *_strcat(char *dest, char *src);
 
 /* function concatenates two strings*/
 char *_strncat(char *dest, char *src, int n);
+
+/*function that copies a string*/
+char *_strncpy(char *dest, char *src, int n);
+
