7d5bc6cebed149292ca50e04bb99bd05293b1110
Rickyflame <kimumuricky@gmail.com>
2022-04-22 15:04:38 +0300

    Write a function that compares two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,6 @@ char *_strncat(char *dest, char *src, int n);
 /*function that copies a string*/
 char *_strncpy(char *dest, char *src, int n);
 
+/*function that compares two strings*/
+int _strcmp(char *s1, char *s2);
+
