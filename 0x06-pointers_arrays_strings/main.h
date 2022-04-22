eaa016e6e65499a03faf65f6d82c2d269187d8ce
Rickyflame <kimumuricky@gmail.com>
2022-04-22 13:55:17 +0300

    Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,6 @@ int _putchar (char c);
 
 /* function that concatenates 2 strings*/
 char *_strcat(char *dest, char *src);
+
+/* function concatenates two strings*/
+char *_strncat(char *dest, char *src, int n);
