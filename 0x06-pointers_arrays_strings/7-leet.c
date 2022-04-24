a683cb234fd2d32168f5962c067319bae12cb6f7
Rickyflame <kimumuricky@gmail.com>
2022-04-24 16:26:33 +0300

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 char *leet(char *s)
 {
 	int count = 0, i;
-	int low_letters[] = {97, 101, 11, 116, 108};
+	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
