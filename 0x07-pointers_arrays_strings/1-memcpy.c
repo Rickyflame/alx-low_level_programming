b6befc60af3356d225ddecd2c12bc9e8bf4f6890
Rickyflame <kimumuricky@gmail.com>
2022-04-26 15:43:45 +0300

    Write a funtion tha copies mt copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area from src to dest
- * @dest: destination memory area
- * @src: source memory area
- * @n: bytes filled
- * return: the pointer to dest
+ *_memcpy - copy number bytes form address "from" to address "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
