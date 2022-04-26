f60006453f5c2cebb955ba893fe4e7603196ba72
Rickyflame <kimumuricky@gmail.com>
2022-04-26 14:03:20 +0300

    Write a function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,3 +10,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 /* function that locates a character in a string*/
 char *_strchr(char *s, char c);
 
+/*function that gets the length of a prefix*/
+unsigned int _strspn(char *s, char *accept);
+
