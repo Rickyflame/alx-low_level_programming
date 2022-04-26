0414a21f5e89aa05958f513d40a41b68fc3b688b
Rickyflame <kimumuricky@gmail.com>
2022-04-26 12:01:34 +0300

    Write a function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - funciton copies bytes from memory
+ * @dest: memory area
+ * @src: source memory file
+ * @n: byte size
+ * return: the pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,7 @@ int _putchar (char c);
 
 /*function that filss memeory with a constant byte*/
 char *_memset(char *s, char b, unsigned int n);
- 
+
+/*function that copies memory area*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+
