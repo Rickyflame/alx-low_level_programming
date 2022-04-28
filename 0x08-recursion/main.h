9178d0c5358ea55bcc82e597dc02b5e36b909257
Rickyflame <kimumuricky@gmail.com>
2022-04-28 11:56:26 +0300

    Write a function that returns the length of a string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,3 +7,6 @@ void _puts_recursion(char *s);
 /*function prints a string in reverse*/
 void _print_rev_recursion(char *s);
 
+/*function that returns the length of a string*/
+int _strlen_recursion(char *s);
+
