5d086c652c2a50b0f466450350d80549238b17a0
Rickyflame <kimumuricky@gmail.com>
2022-05-24 13:56:32 +0300

    Unnecessary files

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("(A tortiose, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
