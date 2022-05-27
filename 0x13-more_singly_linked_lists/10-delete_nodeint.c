01837054ad3422704894797c9d90db91297c645e
Rickyflame <kimumuricky@gmail.com>
2022-05-27 21:42:13 +0300

    Write a function that deletes the node at index index of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * delete_nodeint_at_index - Deletes the node at given index of a list
+ * @head: Memory address of the linked list
+ * @index: Index of the list to search
+ * Return: 1 on success, -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	listint_t *previous;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	while (temp != NULL && i != index)
+	{
+		previous = temp;
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (-1);
+	previous->next = temp->next;
+	free(temp);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -42,9 +42,13 @@ int pop_listint(listint_t **head);
 
 /*function that returns the nth node of a linked list*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
 /*function that returns the sum of all data*/
 int sum_listint(listint_t *head);
 
 /*function that inserts a new node at a given postion*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+/*Write a function that deletes the node at index index of a listint_t linked list*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #endif
