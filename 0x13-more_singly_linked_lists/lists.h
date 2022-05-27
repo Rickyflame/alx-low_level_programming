391490b9748e827fb7df073c588046c9cf4f52fa
Rickyflame <kimumuricky@gmail.com>
2022-05-27 21:25:14 +0300

    Write a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -45,4 +45,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /*function that returns the sum of all data*/
 int sum_listint(listint_t *head);
 
+/*function that inserts a new node at a given postion*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 #endif
