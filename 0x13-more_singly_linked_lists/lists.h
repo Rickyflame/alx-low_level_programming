0efa0a98c9be7aa779c9b7d660b4b3515580fb37
Rickyflame <kimumuricky@gmail.com>
2022-05-27 23:10:30 +0300

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -49,7 +49,7 @@ int sum_listint(listint_t *head);
 /*function that inserts a new node at a given postion*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
-/*Write a function that deletes the node at index index of a listint_t linked list*/
+/*Write a function that deletes the node at index linked list*/
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 /*Write a function that reverses a listint_t linked list*/
