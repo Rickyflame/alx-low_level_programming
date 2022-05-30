395427d7aa8e259a16a1c57174e692645a52a715
Rickyflame <kimumuricky@gmail.com>
2022-05-30 17:13:00 +0300

    Wrtie a function that prints the binary representation of a number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,9 @@
+/* putchar function*/
+int _putchar(char c);
+
 /*function that converts a binary number to an unsigned int*/
 unsigned int binary_to_uint(const char *b);
+
+/*function that prints the binary representation of a number*/
+void print_binary(unsigned long int n);
 
