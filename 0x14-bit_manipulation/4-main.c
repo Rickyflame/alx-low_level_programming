e2208269cf05e62bf50cc23ff4c79f2b921a1642
Rickyflame <kimumuricky@gmail.com>
2022-06-02 10:58:26 +0300

    Unneccessary files

---
+++ 0x14-bit_manipulation/4-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include"main.h"
+
+/**
+ * main - checks the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int n;
+
+	n = 1204;
+	clear_bit(&n, 10);
+	printf("%lu\n", n);
+	n = 0;
+	clear_bit(&n, 10);
+	printf("%lu\n", n);
+	n = 98;
+	clear_bit(&n, 1);
+	printf("%lu\n", n);
+	n = 98;
+	clear_bit(&n, n);
+	printf("%lu\n", n);
+	return (0);
+}
