7f660f27a546e218c91450269077b8244993580e
Rickyflame <kimumuricky@gmail.com>
2022-06-02 11:22:24 +0300

    File I/O

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*_putchar function*/
+int _putchar(char c);
+
+/*function that reads a text file and prints it*/
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
