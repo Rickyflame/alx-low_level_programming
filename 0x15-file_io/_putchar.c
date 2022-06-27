c0da8d430fbf9e36e905dd8b51ae4b31e2531a6e
Rickyflame <kimumuricky@gmail.com>
2022-06-27 09:31:23 +0300

    Unnecessary files

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,12 @@
+#Include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success1
+ * On error, -1 is returned, and erno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
