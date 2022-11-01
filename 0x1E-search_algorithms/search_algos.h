7f7decec82eb1742974c780d7ae93a8218e998f5
Rickyflame <kimumuricky@gmail.com>
2022-11-01 11:04:36 +0300

    Unnecessary file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,44 @@
+#ifndef _SORTING_ALGOS_H
+#define _SORTING_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @index: index of node in the list
+ * @next: pointer to the next node
+ */
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+/**
+ * struct skiplist_s - singly linked list with an express lane
+ * @n: integer
+ * @index: index of node in the list
+ * @next: pointer to the next noode
+ * @express: pointer ti the next node in the express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct akiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif
